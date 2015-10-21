b7419ca20229c3b58e6f19329f2fd0ddecd8643e
Thomas Zimmermann <tdz@users.sourceforge.net>
2015-10-21 12:39:54 +0200

    Bug 1207480: Include B2G Bluedroid configuration from libhardware_moz

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -85,9 +85,12 @@ WPA_SUPPLICANT_VERSION := VER_0_8_X
 
 # BT definitions for Qualcomm solution
 BLUETOOTH_HCI_USE_MCT := true
-BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/sony/yukon/bluetooth
 BOARD_HAVE_BLUETOOTH := true
 BOARD_HAVE_BLUETOOTH_QCOM := true
+
+BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := \
+  device/sony/yukon/bluetooth \
+  hardware/libhardware_moz/include/hardware_moz/bluetooth/bluedroid
 
 # GPS definitions for Qualcomm solution
 BOARD_VENDOR_QCOM_GPS_LOC_API_HARDWARE := $(TARGET_BOARD_PLATFORM)
